2d7733c0137dca9982cec7087fd4dedd1f8851b3
SamyShaawat <samy.shaawat@gmail.com>
2024-03-24 10:52:25 +0200

    update and finish navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -139,8 +139,8 @@ const Navbar = () => {
     <Nav>
       <NavbarContainer>
         <NavLogo to="/">
-          <ColorText>&lt;</ColorText>Rishav
-          <div style={{ color: theme.primary }}>/</div>Chanda
+          <ColorText>&lt;</ColorText>Samy
+          <div style={{ color: theme.primary }}>/</div>Mostafa
           <ColorText>&gt;</ColorText>
         </NavLogo>
 
